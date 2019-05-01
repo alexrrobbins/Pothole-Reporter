24eff594ee96c624ce5d268be67873462aee8e97
Alex Robbins <alexrrobbins@gmail.com>
2019-05-01 15:23:50 -0400

    Use Current Location button now writes data to the file on click

---
+++ coords.py
@@ -0,0 +1,10 @@
+class Coords():
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def get_lat(self):
+        return self.latitude
+
+    def get_long(self):
+        return self.longitude
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 from flask import Flask, render_template, request, redirect, Response, jsonify
+from coords import Coords
 
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
@@ -9,6 +10,7 @@ db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 app = Flask(__name__)
+coordinates = ''
 
 @app.route('/')
 def index():
@@ -17,15 +19,32 @@ def index():
 @app.route('/get_coords',methods=['GET','POST'])
 def get_coords():
     if request.method == 'POST':
-        print('Incoming..')
         print(request.get_json())  # parse as JSON
-        print(request.get_json()['lat'])
-        print(request.get_json()['lng'])
+        latitude = request.get_json()['lat']
+        longitude = request.get_json()['lng']
+        global coordinates
+        coordinates = Coords(latitude, longitude)
         return 'OK', 200
     return render_template(
         'result.html',
         latitude="testing",
         longitude="test")
+
+@app.route('/save_coords')
+def save_coords():
+    global coordinates
+    latitude = coordinates.get_lat()
+    longitude = coordinates.get_long()
+    coordsfile = open('coordsdata.txt','a')
+    coordsfile.write('Latitude: ')
+    coordsfile.write(str(latitude))
+    coordsfile.write('\n')
+    coordsfile.write('Longitude: ')
+    coordsfile.write(str(longitude))
+    coordsfile.write('\n')
+    coordsfile.close()
+    print('File write complete')
+    return 'OK', 200
 
 @app.errorhandler(404)
 def page_not_found(e):
