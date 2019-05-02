d1e85778f39c291c1dcde03913ecbf45c37363b6
Alex Robbins <alexrrobbins@gmail.com>
2019-05-02 16:15:29 -0400

    Got rid of the script that kept breaking, now it seems to work every time

--- static/js/initmap.js
+++ static/js/initmap.js
@@ -9,6 +9,20 @@
     var coordsjson = initialLocation.toJSON();
     map.setCenter(initialLocation);
     map.setZoom(12);
+    $.ajax({
+                type: 'POST',
+                contentType: 'application/json',
+                data:  JSON.stringify(coordsjson),
+                dataType: 'json',
+                url: '/get_coords',
+                success: function (e) {
+                    console.log(e);
+                    window.location = "/get_coords";
+                },
+                error: function(error) {
+                console.log(error);
+            }
+            });
   }, function(positionError) {
     map.setCenter(new google.maps.LatLng(41.201992, -75.913101));
     map.setZoom(8);
