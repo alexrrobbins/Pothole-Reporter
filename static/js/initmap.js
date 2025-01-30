d8cff0cbc4c03bbd226e4ed048779928a35f06af
alexrrobbins <alexrrobbins@gmail.com>
2025-01-30 18:54:42 -0500

    Updated JS for map initialization

--- static/js/initmap.js
+++ static/js/initmap.js
@@ -1,28 +1,42 @@
-//Code taken from https://stackoverflow.com/questions/17382128/google-maps-api-center-map-on-clients-current-location/17383412
-  var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'));
-    navigator.geolocation.getCurrentPosition(function(position) {
-    var latitude = position.coords.latitude
-    var longitude = position.coords.longitude
+//Code taken from https://stackoverflow.com/questions/17382128/google-maps-api-center-map-on-clients-current-location/17383412 and fixed with CoPilot's help
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=<insertkeyhere>&callback=initMap';
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
+window.onload = loadScript;
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.201992, lng: -75.913101 },
+    zoom: 8
+  });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
     var initialLocation = new google.maps.LatLng(latitude, longitude);
     var coordsjson = initialLocation.toJSON();
     map.setCenter(initialLocation);
     map.setZoom(12);
     $.ajax({
-                type: 'POST',
-                contentType: 'application/json',
-                data:  JSON.stringify(coordsjson),
-                dataType: 'json',
-                url: '/get_coords',
-                success: function (e) {
-                    console.log(e);
-                    window.location = "/get_coords";
-                },
-                error: function(error) {
-                console.log(error);
-            }
-            });
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(coordsjson),
+      dataType: 'json',
+      url: '/get_coords',
+      success: function (e) {
+        console.log(e);
+        window.location = "/get_coords";
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
   }, function(positionError) {
     map.setCenter(new google.maps.LatLng(41.201992, -75.913101));
     map.setZoom(8);
